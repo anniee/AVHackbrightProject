50a2c92d70af2ebe41d5f2092f1f1fbbe364c2a2
Anne Vetto <vettoap@gmail.com>
2015-02-17 17:01:40 -0800

    Initial Commit

---
+++ make_botgal.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+#route to handle the landing page of the BotGals website
+@app.route('/')
+def welcome_page():
+	#Eventually, put in html/css of rocket ship symbol to click to enter main page /makestation
+	
+	return render_template("home_page.html")
+
+	#return "Welcome to the World of BotGals. ~~~Making robot gals since 2015~~~"
+
+
+@app.route('/makestation')
+def make_galbot():
+	return render_template("main_page.html")
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)

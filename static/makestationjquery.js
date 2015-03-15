126dc2089dc5a1bfff17bc65c34d2cb7951d0a92
Anne Vetto <vettoap@gmail.com>
2015-03-14 17:55:47 -0700

    bootstrap link working

--- static/makestationjquery.js
+++ static/makestationjquery.js
@@ -1,8 +1,23 @@
+// $("face").on("click", function() {
+//   $("#progress").css("visibility", "visible")
+//   });
+
 
 
 // $(document).ready(function() {
+// 	$("face").on("click", function() {
+// 		$("#progress").css("visibility", "visible")
+// 	});
+// });
 
 // 	$(".bxslider").click(function() {
 // 		alert("face selected");
 // 	});
+// });
+
+
+// 	$( "#clickme" ).click(function() {
+// $( "#book" ).show( "slow", function() {
+// // Animation complete.
+// });
 // });
